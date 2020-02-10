dad5a0589e1bcfb6dac42b99a58af5fe906e1a16
Kareem Rolle <kareemrolle@yahoo.com>
2020-02-10 00:50:39 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,40 +1,25 @@
-require 'pry'
+class Genre < Super
 
- class Genre
-   extend Concerns::Findable
-   attr_accessor :name
-   attr_reader :songs
-
-   @@all = []
+   attr_accessor :songs
 
    def initialize(name)
      @name = name
      @songs = []
    end
 
-   def self.all
-     @@all
-   end
-
-   def self.destroy_all
-     @@all.clear
-   end
-
-   def save
-     @@all << self
-   end
-
    def self.create(name)
-     genre = self.new(name)
-     genre.save
-     genre
-   end
+ 		instance = Genre.new(name)
+ 		instance.save
+ 		instance
+ 	end
 
-   def songs
-     @songs
+   def add_song(song)
+     song.genre = self if song.genre == nil
+     self.songs << song if self.songs.include?(song) == false
    end
 
    def artists
-     self.songs.collect {|s| s.artist}.uniq
+     self.songs.collect{|song|song.artist}.uniq
    end
+
  end
