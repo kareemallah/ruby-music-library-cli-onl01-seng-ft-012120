274834e5a9010427d8187ab065c6898d3a9e57f1
Kareem Rolle <kareemrolle@yahoo.com>
2020-02-10 00:50:27 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -1,19 +1,22 @@
-require 'pry'
+class MusicImporter
 
- class MusicImporter
-   attr_accessor :path, :files
+ 	attr_accessor :path
 
-   def initialize(path)
-     @path = path
-   end
+ 	def initialize(path)
+ 		@path = path
+ 	end
 
-   def files
-       @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
-   end
+ 	def files
+ 		files = Dir.glob("#{path}**/*")
+ 		files = files.collect{|file|file.scan(/(?<=mp3s\/)(.*)/)}.flatten #disgusting RegEx that matches everything after the path
+ 	end
 
-   def import
-     files.each {|file| Song.create_from_filename(file) }
-   end
+ 	def import
+ 		new_files = self.files
+ 		new_files.each do |file|
+ 			new_song = Song.create_from_filename(file)
+ 		end
 
+ 	end
 
  end
