bc5a807e0016d30d2e9f7368b0ef563f87656000
Kareem Rolle <kareemrolle@yahoo.com>
2020-02-10 00:50:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,48 +1,25 @@
-require 'pry'
+class Artist < Super
 
- class Artist
-   extend Concerns::Findable
-   attr_accessor :name
-   attr_reader :songs
-
-   @@all = []
+   attr_accessor :songs
 
    def initialize(name)
-     @name = name
+ 		@name = name
      @songs = []
-     # self.save
-   end
-
-   def self.all
-     @@all
-   end
-
-   def self.destroy_all
-     @@all.clear
-   end
-
-   def save
-     @@all << self
-   end
+ 	end
 
    def self.create(name)
-     artist = self.new(name)
-     artist.save
-     artist
-   end
+ 		instance = Artist.new(name)
+ 		instance.save
+ 		instance
+ 	end
 
-   def songs
-     @songs
+   def genres
+     self.songs.map {|song|song.genre}.uniq
    end
 
    def add_song(song)
-
-       song.artist = self unless song.artist
-       songs << song unless songs.include?(song)
-
+     song.artist = self if song.artist == nil
+     self.songs << song if self.songs.include?(song) == false
    end
 
-   def genres
-     songs.collect { |song| song.genre}.uniq
-   end
  end
